14ba247d42a3756c70e5950d8d8423644d2bb848
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-07-08 17:18:37 -0700

    Added High and low temperature for forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,12 @@
 const request = require("request");
 
+/**
+ * Goal: Add new data to forecast: temperature high and low
+ *  1. update the forecast string to include new data
+ *  2. commit changes
+ *  3. push and deploy to heroku
+ */
+
 const forecast = (latitude, longitude, callbackfunction) => {
   const url =
     "https://api.darksky.net/forecast/45c8fd671c3c1dbd4b83dcc55bc51803/" +
@@ -14,12 +21,17 @@ const forecast = (latitude, longitude, callbackfunction) => {
     } else if (body.error) {
       callbackfunction("Unable to find location", undefined);
     } else {
+      //console.log(body.daily.data[0])
       callbackfunction(
         undefined,
         body.daily.data[0].summary +
           " The temperature outside is currently " +
           body.currently.temperature +
-          " degrees out. There us a " +
+          " degrees out. The high today is " +
+          body.daily.data[0].temperatureHigh +
+          " with a low of " +
+          body.daily.data[0].temperatureLow +
+          ". There is a " +
           body.currently.precipProbability +
           "% chance of rain."
       );
